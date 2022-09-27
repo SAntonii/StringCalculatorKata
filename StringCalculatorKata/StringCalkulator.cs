f811eddccea2ba506f2078356db6074960c997b6
Szymon.Antoniuk <Szymon.Antoniuk@rossmann.com.pl>
2022-09-27 11:40:51 +0200

    Add project files.

---
+++ StringCalculatorKata/StringCalkulator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public static class StringCalkulator
+    {
+
+
+        public static decimal Add(string a)
+        {
+            if (String.IsNullOrEmpty(a))
+            {
+                return 0;
+            }
+            string delimiter = ",";
+
+
+            var adelimiter = a.Split("\n");
+            if (adelimiter[0] != null && adelimiter[0].Contains("//"))
+            {
+                var bdelimiter = adelimiter[0].Split("//");
+                delimiter = bdelimiter[1];
+
+                a = a.Replace(adelimiter[0], "");
+                a = a.Remove(0, 1);
+               var regexList = Regex.Match(delimiter, @"\[(.*?)\]");
+                delimiter = regexList.Value;
+                delimiter = delimiter.Replace("[", "");
+                delimiter = delimiter.Replace("]", "");
+            }
+
+            a = a.Replace("\n", delimiter);
+            var split = a.Split(delimiter);
+
+            if (split.Length == 0)
+            {
+                var zzz = decimal.Parse(a);
+                if (zzz > 999)
+                    return 0;
+                return zzz;
+            }
+            decimal sum = 0;
+            var minusList = new List<decimal>();
+            foreach (var item in split)
+            {
+                var ditem = decimal.Parse(item);
+                if (ditem < 0)
+                    minusList.Add(ditem);
+
+                if (ditem < 1000)
+                {
+                    sum += ditem;
+                }
+            }
+            if (minusList.Any())
+            {
+                throw new ArgumentException("negatives not allowed :" + String.Join(delimiter, minusList));
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ TestNUnit/UnitTest1.cs
@@ -0,0 +1,93 @@
+using StringCalculatorKata;
+
+namespace TestNUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //[Test]
+        //public void Test1()
+        //{
+        //    Assert.Pass();
+        //}
+
+        [TestCase("2,2",4)]
+        public void TestAdd1(string s , int v)
+        {
+           
+
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+        [TestCase("2", 2)]
+        public void TestAdd2(string s, int v)
+        {
+
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+
+        [TestCase("", 0)]
+        public void TestAdd3(string s, int v)
+        {
+
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+
+        [TestCase("2,2,4,4,5,8", 25)]
+        public void TestAdd4(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+        [TestCase("1\n2,3", 6)]
+        public void TestAdd5(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+        [TestCase("//[test]\n1test2", 3)]
+        public void TestAdd6(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+
+        [TestCase("2,-2,4,-4,5,8", 25)]
+        public void TestAdd7(string s, int v)
+        {
+
+            Assert.That(() => StringCalkulator.Add(s), Throws.TypeOf<ArgumentException>()
+                .With
+                .Message
+                .EqualTo("negatives not allowed :-2,-4"));
+        }
+        [TestCase("1000", 0)]
+        public void TestAdd8(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+
+        [TestCase("2,1000,345,3452,612", 959)]
+        public void TestAdd9(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+
+        [TestCase("//[***]\n1***2***3", 6)]
+        public void TestAdd10(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+
+        [TestCase("//[***]\n1***2***3", 6)]
+        public void TestAdd11(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+        [TestCase("//[***][%%%]\n1***2%%%3", 6)]
+        public void TestAdd12(string s, int v)
+        {
+            Assert.That(() => StringCalkulator.Add(s), Is.EqualTo(v));
+        }
+    }
+}
